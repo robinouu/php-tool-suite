667129c9facf12ee07587e42293ac7d74bd3aebc
Robin Ruaux <robin.ruaux@gmail.com>
2015-04-29 13:40:32 +0200

    ivs are now concatened to content for portability

--- lib/crypto.inc.php
+++ lib/crypto.inc.php
@@ -6,30 +6,28 @@
 require_once('var.inc.php');
 require_once('random.inc.php');
 
-if( !var_get('crypto/iv') ){
-	if( extension_loaded('mcrypt') ){
-		var_set('crypto/iv', $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_RAND));
-	}
-}
-
 function generate_key($key) {
 	return hash("SHA256", $key, true);
 }
 
 function encrypt(&$content, $key){
-	if( !var_get('crypto/iv') ){
+	if( !extension_loaded('mcrypt') ){
 		return false;
 	}
-	$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $content, MCRYPT_MODE_CBC, var_get('crypto/iv'));
-	$content = rtrim(base64_encode($encrypted), "\0\3");
+	$iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_RAND);
+	$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $content, MCRYPT_MODE_CBC, $iv);
+	$content = $iv . rtrim(base64_encode($encrypted), "\0\3");
 	return (bool)$encrypted;
 }
 
 function decrypt(&$content, $key) {
-	if( !var_get('crypto/iv') ){
+	if( !extension_loaded('mcrypt') ){
 		return false;
 	}
-	$decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, base64_decode($content), MCRYPT_MODE_CBC, var_get('crypto/iv'));
+	$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+	$iv = substr($content, 0, $iv_size);
+	$to_decrypt = substr($content, $iv_size);
+	$decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, base64_decode($to_decrypt), MCRYPT_MODE_CBC, $iv);
 	$content = rtrim($decrypted, "\0\3");
 	return (bool)$decrypted;
 }
