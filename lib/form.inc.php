19de190dcff9e9bfc0eecd6b8e7e211f1282651a
Robin Ruaux <robin.ruaux@gmail.com>
2015-05-01 16:17:31 +0200

    add csrf token helpers

--- lib/form.inc.php
+++ lib/form.inc.php
@@ -4,15 +4,33 @@
  * @package php-tool-suite
  */
 
-plugin_require(array('i18n', 'html'));
+function form_token($tokenName) {
+	$token = array(
+		'time' => time(),
+		'salt' => sha1(uniqid().time()),
+		'sessid' => session_id(),
+		'ip' => $_SERVER['REMOTE_ADDR']
+	);
+	
+	session_var_set('csrf_token/'.$tokenName, $token);
 
-function form_errors($errors) {
-	$c = array();
-
-	foreach ($errors as $key => $errs) {
-		$c[] = implode(br(), $errs);
-	}
-	return tag('div', implode(br(), $c), array('class' => 'msg msg-error'));
+	$hash = object_hash($token);
+	return base64_encode($hash);
 }
 
+function form_token_validate($tokenName, $timeout = 300, $csrf = null) {
+	$token = session_var_get('csrf_token/' . $tokenName);
+	if( $token ){
+		if( $_SERVER['REQUEST_TIME'] - $token['time'] < $timeout ){
+			if( session_id() ){
+				if( !$csrf ){
+					$csrf = $_REQUEST['csrf'];
+					$tokenHash = base64_decode($csrf);
+					return object_hash($token) === $tokenHash;
+				}
+			}
+		}
+	}
+	return FALSE;
+}
 ?>
