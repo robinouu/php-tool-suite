0dce6e0255f6e81b047065349281857b163dfae3
Robin Ruaux <robin.ruaux@gmail.com>
2015-05-04 07:31:08 +0200

    model: add proxy object for advanced model queries

--- lib/model.inc.php
+++ lib/model.inc.php
@@ -51,12 +51,12 @@ function models_to_sql(&$models) {
 						'hasID' => false,
 						'columns' => array(
 							'id_' . $tableName . ' int(11) NOT NULL',
-							'id_' . $fieldName . ' int(11) NOT NULL'
+							'id_' . $field['data'] . ' int(11) NOT NULL'
 						),
-						'primaryKeys' => array('id_' . $tableName . ',id_' . $fieldName),
+						'primaryKeys' => array('id_' . $tableName . ',id_' . $field['data']),
 						'foreignKeys' => array(
-							'id_' . $tableName => array('name' => 'FK_id_' . $manyTableName . '_' . $fieldName, 'ref' => $prefix . $tableName.'(id)'),
-							'id_' . $fieldName => array('name' => 'FK_id_' . $manyTableName . '_' . $field['data'], 'ref' => $prefix . $field['data'].'(id)')
+							'id_' . $tableName => array('name' => 'FK_id_' . $manyTableName . '_' . $tableName, 'ref' => $prefix . $tableName.'(id)'),
+							'id_' . $field['data'] => array('name' => 'FK_id_' . $manyTableName . '_' . $field['data'], 'ref' => $prefix . $field['data'].'(id)')
 						)
 					);
 
@@ -115,111 +115,291 @@ function model_name($model) {
 	return isset($model['labels']['singular']) ? $model['labels']['singular'] : ucfirst($model['id']);
 }
 
-function model_select_fields($fields, $aliasPrefix = '') {
-	$back = array();
-	foreach ($fields as $fieldName => $field) {
-		$field = array_merge(var_get('field/default', array()), $field);
-		if( $field['type'] !== 'relation' ){
-			$back[] = $fieldName . ' AS ' . sql_quote($aliasPrefix . $fieldName, true);
-		}
-	}
-	return $back;
-}
 
-function model_get(&$models, $modelName, $options = array()){
+class Model {
 	
+	static public $schema = array();
 
-	$model = $models[$modelName];
-	$tableName = isset($model['table']) ? $model['table'] : $modelName;
-	$selects = array();
-	$joins = array();
-	foreach( $model['fields'] as $fieldName => $field) {
-		$field = array_merge(var_get('field/default', array()), $field);
+	protected $selects = array();
+	protected $aliases = array();
+	protected $using = array();
+	protected $joins = array();
+	protected $filters = array();
+	protected $groupBy = array();
+	protected $orderBy = array();
+	protected $limit = null;
+	protected $offset = null;
+
+	protected $insertions = array();
+	protected $replacements = array();
+
+	private $modelName;
+	private $fields;
+
+
+	const REGEX_MODEL_PATH = '#(?:`((?:[^`]|``)+)`)|([^`\.]+)#'; // matches model paths
+
+	public function __construct($modelName) {
+
+		$this->modelName = $modelName;
+
+		$model = &Model::$schema[$modelName];
+
+		$tableName = Model::getTableName($modelName);
+
+		foreach( $model['fields'] as $fieldName => $field) {
+			$this->fields[$fieldName] = $field = array_merge(var_get('field/default', array()), $field);
+		}
+		return $this;
+	}
+
+	static public function getTableName($modelName) {
+		$model = &Model::$schema[$modelName];
+		return isset($model['table']) ? $model['table'] : $modelName;
+	}
+	
+	public function select($select) {
+		if( is_string($select) ){
+			$select = array($select);
+		}
+		$this->selects = array_merge($this->selects, $select);
+		return $this;
+	}
+
+	public function groupBy($groupBy) {
+		if( is_string($groupBy) ){
+			$groupBy = array($groupBy);
+		}
+		$this->groupBy = array_merge($this->groupBy, $groupBy);
+		return $this;
+	}
+
+	public function orderBy($orderBy) {
+		if( is_string($orderBy) ){
+			$orderBy = array($orderBy);
+		}
+		$this->orderBy = array_merge($this->orderBy, $orderBy);
+		return $this;
+	}
+
+	public function limit($limit) {
+		$this->limit = $limit;
+		return $this;
+	}
+
+	public function offset($offset) {
+		$this->offset = $offset;
+		return $this;
+	}
+
+	private function resolveFieldByPath($modelPath) {
+		$parent = $this->modelName;
+		$field = null;
+		foreach ($modelPath as $path ) {
+			$field = &Model::$schema[$parent]['fields'][$path];
+			$parent = $field['data'];
+		}
+		return $field;
+	}
+
+	public function using($modelPath, $alias = null, $selectFields = false) {
 		
-
-		if( $field['type'] === 'relation' ){
-			if( !$field['hasMany'] ){
-				$joins[] = array(
-					'type' => ($field['required'] ? 'INNER JOIN' : 'LEFT JOIN'),
-					'tableRight' => $field['data'],
-					'aliasRight' => $fieldName,
-					'columnRight' => 'id',
-					'columnLeft' => $fieldName,
-				);
-				$selects = array_merge($selects, model_select_fields($models[$fieldName]['fields'], $fieldName . '.'));
-			}else{
-				$joins[] = array(
-					'tableRight' => $tableName . '_' . $fieldName,
-					'columnRight' => 'id_' . $tableName,
-				);
-				$joins[] = array(
-					'tableLeft' => $tableName . '_' . $fieldName,
-					'columnLeft' => 'id_' . $fieldName,
-					'tableRight' => $field['data'],
-					'aliasRight' => $fieldName,
-					'columnRight' => 'id'
-				);
-				$selects = array_merge($selects, model_select_fields($models[$field['data']]['fields'], $fieldName . '.'));
+		if( is_string($modelPath) ){
+			if( preg_match_all(Model::REGEX_MODEL_PATH, $modelPath, $matches) ){
+				$modelPath = array();
+				foreach( $matches[1] as $k => $v ) {
+					$modelPath[] = $matches[1][$k] ? $matches[1][$k] : $matches[2][$k];
+				}
 			}
-		}else{
-			$selects[] = sql_quote($fieldName, true);
+
 		}
-	}
-	
-	$finalOptions['select'] = array(implode(', ', $selects));
-	$finalOptions['join'] = $joins;
-	$finalOptions['alias'] = $modelName;
 
-	$finalOptions = array_merge($finalOptions, $options);
+		if( is_array($modelPath) ){
 
-	return sql_get($modelName, $finalOptions);
-}
+			if( ($len = sizeof($modelPath)) ){
+				
+				$columnName = array_pop($modelPath);
+				$parentModelField = $this->resolveFieldByPath($modelPath);
+				$parentModelName = $parentModelField ? $parentModelField['data'] : $this->modelName;
+				$parentTableName = Model::getTableName($parentModelName);
 
-
-function model_insert(&$models, $modelName, $datas){
-
-	$model = $models[$modelName];
-	$tableName = isset($model['table']) ? $model['table'] : $modelName;
-	$relations_id = array();
-
-	foreach( $model['fields'] as $fieldName => $field) {
-		$field = array_merge(var_get('field/default', array()), $field);
-		
-		if( $field['type'] === 'relation' && isset($datas[$fieldName]) && is_array($data = $datas[$fieldName]) ){
-			$relation_id = null;
-			
-			if( !$field['hasMany'] ){
-				$relation_id = model_insert($models, $field['data'], $data);
-			}else{
-				$relations_id[$fieldName] = array();
-				if( is_assoc_array($data) ){
-					$relationsData = array($data);
+				$modelPath[] = $columnName;
+				$usingModelField = $this->resolveFieldByPath($modelPath);
+				$usingModelName = $usingModelField['data'];
+				$usingTableName = Model::getTableName($usingModelName);
+				
+				if( !$usingModelField['hasMany'] ){
+					$this->join(array(
+						'type' => 'JOIN',
+						'tableLeft' => $parentTableName,
+						'aliasLeft' => isset($this->aliases[$parentTableName]) ? $this->aliases[$parentTableName] : '',
+						'columnLeft' => $columnName,
+						'tableRight' => $usingTableName,
+						'aliasRight' => $alias,
+						'columnRight' => 'id'
+					));
 				}else{
-					$relationsData = &$data;
+					$this->join(array(
+						'tableRight' => $parentTableName . '_' . $columnName,
+						'columnRight' => 'id_' . $parentTableName,
+					));
+					$this->join(array(
+						'tableLeft' => $parentTableName . '_' . $columnName,
+						'columnLeft' => 'id_' . $usingModelName,
+						'tableRight' => $usingModelField['data'],
+						'aliasRight' => $alias ? $alias : implode('.', $modelPath),
+						'columnRight' => 'id'
+					));
 				}
-				foreach( $relationsData as $relationData ) {
-					$relations_id[$fieldName][] = model_insert($models, $field['data'], $relationData);
+
+				if( $alias ){
+					$this->aliases[$usingTableName] = $alias;
 				}
-			}
-			unset($datas[$fieldName]);
-			if( $relation_id ){
-				$datas[$fieldName] = $relation_id;
+
+				if( $selectFields ){
+					$fields = Model::$schema[$usingTableName]['fields'];
+					foreach ($fields as $fieldName => $field) {
+						$field = array_merge(var_get('field/default', array()), $field);
+						if( $field['hasMany'] ) {
+							continue;
+						}
+						$select = sql_quote($alias ? $alias : $usingTableName, true);
+
+						$select .= '.' . sql_quote($fieldName, true);
+						if( $alias ){
+							$select .= ' AS ' . sql_quote($alias . '.' . $fieldName, true);
+						}
+						$this->selects[] = $select;
+					}
+				}
 			}
 		}
+		return $this;
 	}
 
-	sql_insert($tableName, $datas);
-	$id = sql_last_id();
+	public function join($joins) {
+		$this->joins[] = $joins;
+		return $this;
+	}
 
-	if( sizeof($relations_id) ){
-		foreach( $relations_id as $fieldName => $relation_ids ) {
-			foreach ($relation_ids as $relation_id) {
-				sql_insert($tableName . '_' . $fieldName, array('id_' . $tableName => $id, 'id_' . $fieldName => $relation_id));
+	public function where($where) {
+		$this->filters[] = sql_logic($where);
+		return $this;
+	}
+
+	public function get() {
+		$options = array();
+		$options['select'] = implode(', ', $this->selects);
+		$options['join'] = $this->joins;
+		$options['where'] = implode(' AND ', $this->filters);
+		$options['groupBy'] = implode(', ', $this->groupBy);
+		$options['orderBy'] = implode(', ', $this->orderBy);
+		$options['limit'] = $this->limit;
+		$options['offset'] = $this->offset;
+		return sql_get($this->modelName, $options);
+	}
+
+	public function insert(array $data) {
+		$this->insertions[] = $data;
+		return $this;
+	}
+
+	public function replace(array $data) {
+		$this->replaces[] = $data;
+		return $this;
+	}
+
+	public function commit() {
+		if( sizeof($this->insertions) ){
+			$this->inserted_ids = array();
+			foreach ($this->insertions as $index => $insertion) {
+			 	$this->doInsertion($index);
+		 	}
+		}
+		$this->reset();
+		return $this;
+	}
+
+	public function reset() {
+		$this->insertions = array();
+		$this->replaces = array();
+		$this->using = array();
+		$this->aliases = array();
+		$this->joins = array();
+		$this->filters = array();
+		$this->groupBy = array();
+		$this->orderBy = array();
+		$this->limit = null;
+		$this->offset = null;
+		return $this;
+	}
+
+	private function doInsertion($index, $modelName = null, $subKeys = null) {
+
+		$datas = $this->insertions[$index];
+		if( $subKeys ){
+			$datas = var_get($subKeys, null, $datas);
+		}else{
+			$subKeys = array();
+		}
+
+		if( !$modelName ){
+			$modelName = $this->modelName;
+		}
+
+		$model = &Model::$schema[$modelName];
+		$tableName = Model::getTableName($modelName);
+		$fields = &$model['fields'];
+
+		$relations_ids = array();
+
+		foreach( $fields as $fieldName => $field) {
+			$field = array_merge(var_get('field/default', array()), $field);
+			if( $field['type'] === 'relation' && isset($datas[$fieldName]) ){
+				$data = $datas[$fieldName];
+				$relation_id = null;
+				if( !$field['hasMany'] ){
+					$relation_id = $this->doInsertion($index, $field['data'], array_merge($subKeys, array($fieldName)));
+				}else{
+					$relations_ids[$fieldName] = array();
+					if( is_array($data) ){
+						if( is_assoc_array($data) ){
+							$relationsData = array('data' => $data);
+						}else{
+							$relationsData = &$data;
+						}
+						foreach( $relationsData as $key => $relationData ) {
+							if( is_array($relationData) ){
+								$relations_ids[$fieldName][] = $this->doInsertion($index, $field['data'], array_merge($subKeys, is_numeric($key) ? array($fieldName, $key) : array($fieldName)));
+							}else{
+								$relations_ids[$fieldName][] = $relationData;
+							}
+						}
+					}else{
+						$relations_ids[$fieldName][] = $data;
+					}
+				}
+				unset($datas[$fieldName]);
+				if( $relation_id ){
+					$datas[$fieldName] = $relation_id;
+				}
 			}
 		}
+
+		sql_insert($tableName, $datas);
+		$id = sql_last_id();
+
+		if( sizeof($relations_ids) ){
+			foreach( $relations_ids as $fieldName => $relation_ids ) {
+				$this->inserted_ids[$fieldName] = array();
+				foreach ($relation_ids as $relation_id) {
+					sql_insert($tableName . '_' . $fieldName, array('id_' . $tableName => $id, 'id_' . $model['fields'][$fieldName]['data'] => $relation_id));
+					$this->inserted_ids[$fieldName][] = sql_last_id();
+				}
+			}
+		}
+
+		$this->inserted_ids[] = $id;
+		return $id;
 	}
-
-	return $id;
-}
-
-
+};
