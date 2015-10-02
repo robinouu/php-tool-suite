1d491c735de4fff7740716c1e78d506541cded4d
Robin Ruaux <robin.ruaux@gmail.com>
2015-10-02 22:44:42 +0200

    cache: add memcached support

--- lib/cache.inc.php
+++ lib/cache.inc.php
@@ -7,30 +7,65 @@
 
 plugin_require(array('fs'));
 
+if( class_exists('Memcache') ){
+	$memcachedOptions = array_merge(
+		array(
+			'host' => '127.0.0.1',
+			'port' => 11211
+		), var_get('memcached/options')
+	);
+	$service = new Memcache();
+	$service->connect($cacheOptions['host'], $cacheOptions['port']);
+	var_set('cache/currentService', $service);
+}
+
+if( ($cacheDir = var_get('cache/dir')) == null ){
+	var_set('cache/dir', $cacheDir = dirname(__FILE__).'/cache/');
+}
+
+if( !is_dir($cacheDir) ){
+	mkdir_recursive($cacheDir);
+	var_dump($cacheDir);
+}
+
 /**
- * Cache data into a file, up to the specified expiration date.
- * @param string $filepath The cached file path.
- * @param mixed|callable $data the data to cache. It can also be the return of a callback.
+ * Cache data into memory if memcached is installed, or into a JSON file by default.
+ * @param string $resourceID string the resource identifier
+ * @param mixed|callable $data the data to cache.
  * @param string $expire The expiration timestamp. +1 month by default.
  * @return mixed The cached data.
  */
-function cache($filename, $data, $expire = null) {
+function cache_set($resourceID=null, $data, $expire = null) {
 	if( !$expire ) {
 		$expire = strtotime('+1 month');
 	}
-	if (is_file($filename) && filemtime($filename) + $expire < 2*time() ) {
-		return file_get_contents($filename);
+	if( class_exists('Memcache') && ($memcached = var_get('cache/currentService')) != null ){
+		return $memcached->set($resourceID, $data, $expire);
 	}else{
-		if( is_callable($data) ){
-			ob_start();
-			$data();
-			$backData = ob_get_contents();
-			ob_end_clean();
+		$filename = var_get('cache/dir', '') . slug($resourceID) . '.json';
+		if (is_file($filename) && filemtime($filename) + $expire < 2*time() ) {
+			return cache_get($resourceID);
+		}else{
+			file_put_contents($filename, json_encode($data));
+			return true;
 		}
-		else{
-			$backData = &$data;
-		}
-		file_put_contents($filename, $backData);
-		return $backData;
 	}
+	return false;
+}
+
+function cache_get($resourceID=null, $defaultValue=null){
+	$value = null;
+	if( is_null($resourceID) ){
+		return $defaultValue;
+	}
+	if( class_exists('Memcache') ){
+		$memcached = var_get('cache/currentService');
+		return $memcached->get($resourceID);
+	}else{
+		$filename = var_get('cache/dir', '') . slug($resourceID) . '.json';
+		if( is_file($filename) ){
+			$value = json_decode(file_get_contents($filename), true);
+		}
+	}
+	return !is_null($value) ? $value : $defaultValue;
 }
