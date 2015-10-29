d6f7bd274003c0553d5f10cdbd398162e2b87d5e
Robin Ruaux <robin.ruaux@gmail.com>
2015-10-29 13:29:23 +0100

    date: add doc

--- lib/date.inc.php
+++ lib/date.inc.php
@@ -5,6 +5,11 @@
  * @subpackage Date Time
  */
 
+/**
+ * Formats a date using the specified format
+ * @param string $date The date string (see strtotime)
+ * @param string $format The format string (see strftime)
+ */
 function format_date($d = 'now', $format = '%d %B %Y, %H:%M') {
 	$datetime = strtotime($d);
 	return strftime($format, $datetime);
