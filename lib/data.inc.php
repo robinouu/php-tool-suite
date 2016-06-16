5f6d82d357e379a1e0ebfef65dd773cbd1590fc8
Robin Ruaux <robin.ruaux@gmail.com>
2016-06-16 12:18:33 +0200

    data: clean doc

--- lib/data.inc.php
+++ lib/data.inc.php
@@ -12,14 +12,14 @@ class Data {
 
     /* Keeps the state of each attribute  */
     private $name;
-    private $attributes; // CREDITS
-    private $and_filter; // robin + charles + doriane
-    private $or_filter; // charles
-    private $order_by_filter; // doriane
-    private $limit_filter; // charles
-    private $offset_filter; // olivier
+    private $attributes;
+    private $and_filter;
+    private $or_filter;
+    private $order_by_filter;
+    private $limit_filter;
+    private $offset_filter;
 
-    private $filter_index; // inspiration
+    private $filter_index;
 
     /* Creates a new Data instance initialized with $attributes */
     public function __construct($name, $attributes = array()) {
@@ -141,85 +141,3 @@ class Data {
 		return $data;
     }
 }
-
-
-function data_register($table, $datas){
-	$database = var_get('sql/schema');
-	$realData = array();
-	foreach ($database[$name]['fields'] as $key => $field) {
-		if( isset($datas[$key]) ){
-			$realData[$key] = $datas[$key];
-		}
-	}
-	if( sizeof($realData) && sql_insert($name, $realData) ){
-		return sql_last_id();
-	}else{
-		return null;
-	}
-}
-
-function data_set($name, $datas, $where = array()) {
-	if( !is_array($where) && isset($datas['id']) ){
-		$where = array('id' => $datas['id']);
-	}
-
-	$defaultSqlField = var_get('sql/defaultField');
-	$database = var_get('sql/schema');
-	$prefix = var_get('sql/prefix');
-
-	$data = $database[$name];
-	$fields = $data['fields'];
-
-	$d = array();
-	foreach ($fields as $key => $f) {
-		if( !isset($datas[$key]) ){
-			continue;
-		}
-		$f = array_merge($defaultSqlField, $f);
-		
-		if( $f['type'] === 'relation' ){
-			
-		}else{
-			if( isset($datas[$pkey]) ) {
-				$d[$pkey] = $datas[$pkey];
-			}
-		}
-	}
-
-	if( !sizeof($d) ){
-		return false;
-	}
-	
-	return (bool)sql_update($name, $d, 'WHERE ' . sql_where($where));
-}
-
-function data_populate(&$fields, $data){
-	if( !is_array($data) ){
-		return;
-	}
-	//var_dump("populate", $fields, "with", $data);
-	foreach ($fields as $key => $value) {
-		$fields[$key]['value'] = isset($data[$key]) ? $data[$key] : null;
-	}
-	//var_dump("result in", $fields);
-}
-
-function data_validate($dataName, $data, &$validationData = array()) {
-	$schema = var_get('sql/schema');
-
-	foreach ($schema[$dataName]['fields'] as $fieldName => $field) {
-		$field['name'] = $fieldName;
-		$field['data'] = $dataName;
-		$back = array();
-		field_validate($field, isset($data[$fieldName]) ? $data[$fieldName] : field_value($field), $back);
-		$validationData = array_merge_recursive($validationData, $back);
-	}
-
-	return !sizeof($validationData['errors']);
-}
-
-function data($model){
-	$instance = new Data($model);
-	return $instance;
-}
-
